c655392fa2472169615bbe68ae0ff6af0f329b0d
mariyamkhan-git <malodhi14@gmail.com>
2026-07-22 14:17:18 +0500

    feat: add task creation functionality

--- app.py
+++ app.py
@@ -1,5 +1,18 @@
+tasks = []
+
+def add_task(task):
+    tasks.append({
+        "title": task,
+        "completed": False
+    })
+
 def main():
     print("Student Task Manager")
+
+    add_task("Study Git")
+    add_task("Study Django")
+
+    print(tasks)
 
 
 if __name__ == "__main__":
